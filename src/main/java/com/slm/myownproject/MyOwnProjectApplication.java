710846f9e0801a9d7715a561b386ce95baa53c12
suleimin <suleimin@gmail.com>
2023-12-15 22:48:49 +0900

    First of My Project

---
+++ src/main/java/com/slm/myownproject/MyOwnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.slm.myownproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnProjectApplication.class, args);
+	}
+
+}
